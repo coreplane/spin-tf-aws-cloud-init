82d3f8cec71a3cf6e8c435abf245a4ab8ccc08a5
Dan Maas <dmaas@maasdigital.com>
2019-07-09 17:15:20 -0400

    updates for Terraform v0.12

--- ami.tf
+++ ami.tf
@@ -9,6 +9,8 @@ data "aws_ami" "amazon_linux" {
   # us-east-1 = "ami-a4827dc9" # Amazon Linux AMI 2016.03.2 HVM (SSD) EBS-Backed 64-bit
   # us-east-1 = "ami-97785bed" # Amazon Linux AMI 2017.09.1 HVM (SSD) EBS-Backed 64-bit
   most_recent = true
+  owners = ["amazon"]
+
   filter {
     name = "name"
     values = ["amzn-ami-*-x86_64-gp2"]
